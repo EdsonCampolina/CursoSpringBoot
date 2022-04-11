83d9c54ca81a6713f1581bfe8c0921c28e568604
EdsonCampolina <ejcsengsoft@gmail.com>
2022-04-11 00:41:10 -0300

    Tratando exceção de update

--- src/main/java/com/cursojava/curso/services/UserService.java
+++ src/main/java/com/cursojava/curso/services/UserService.java
@@ -1,7 +1,10 @@
 package com.cursojava.curso.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -46,9 +49,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		Optional<User> entity = repository.findById(id);
-		updateData(entity.get(), obj);
-		return repository.save(entity.get());
+		try {
+			Optional<User> entity = repository.findById(id);
+			updateData(entity.get(), obj);
+			return repository.save(entity.get());
+		} catch (NoSuchElementException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	public void updateData(User entity, User obj) {
